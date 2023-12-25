2f95f1fb29d57bf7aa54b213fe454f992608796f
Meow Roar <sareacct91@gmail.com>
2023-12-25 14:35:52 -0800

    Basic server config with 2 end points

---
+++ assets/node/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const writeJsonFile = require('./filesystem.js');
+
+// create instance
+const app = express();
+const port = 5001;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// GET
+app.get('/GET-command', cors(), (req, res) => {
+  res.send({ message: 'Take this string!' });
+});
+
+// POST
+app.post('/POST-command', (req, res) => {
+  const dataJson = req.body;
+
+  console.log(dataJson);
+  res.send({ message: 'I got your data' });
+
+  writeJsonFile(dataJson);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+})
