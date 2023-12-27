48cf75f9ecbcf9371bef70ec01e731ad83956bf7
Meow Roar <sareacct91@gmail.com>
2023-12-27 12:24:44 -0800

    Added logger middleware

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const PORT = 5001;
 
 // Middleware
 app.use(express.static('public'));
+app.use(logger);
 
 // Import the main route handler
 const mainRouter = require('./routes/mainRoute');
 app.use('/', mainRouter);
+
+// Custome middleware
+function logger(req, res, next) {
+  console.log(`Request received: ${req.method} ${req.path}`);
+  next();
+}
 
 // Start the server
 app.listen(PORT, () => {
