5c444dc4c442dc769d66aaa379419156d4c92bb6
Meow Roar <sareacct91@gmail.com>
2024-01-03 14:49:43 -0800

    added route for user to edit their info

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -8,6 +8,7 @@ const controller = require('../controllers/userController');
 
 router.route('/register').post(controller.userRegister);
 router.route('/login').post(controller.userLogin);
+router.route('/:id').patch(controller.userUpdateInfo);
 
 
 module.exports = router;
