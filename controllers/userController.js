7d2f90aea43eae624c09d5fd3e2391f109ceb99b
Meow Roar <sareacct91@gmail.com>
2024-01-04 08:05:24 -0800

    Added userUpdateInfo functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,30 +6,40 @@ const userRegister = async (req, res) => {
   const user = await UsersSchema.create({ ...req.body });
   const token = user.createJWT();
   res.status(StatusCodes.CREATED)
-    .json({ user: {email: user.email}, token },);
+    .json({ user: {userId: user._id}, token },);
 };
 
 const userLogin = async (req, res) => {
   const { email, password } = req.body;
-
   if (!email || !password) {
     throw new BadRequestError('Please provide an email and password');
   }
 
-  const user = await UsersSchema.findOne({email});
-
+  const user = await UsersSchema.findOne({ email });
   if (!user || !await user.checkPassword(password)) {
     throw new UnauthenticatedError('Invalid Credentials');
   }
 
   const token = user.createJWT();
-
-  // const user = await UsersSchema.findOne('')
-  res.status(StatusCodes.OK).json({ user: {email: user.email}, token },);
+  res.status(StatusCodes.OK).json({ user: {userId: user._id}, token },);
 };
 
-const userUpdateInfo = (req, res) => {
-  res.status(StatusCodes.OK).send('upated');
+const userUpdateInfo = async (req, res) => {
+  const { id: userId } = req.params;
+  const { email, password } = req.body;
+
+  if (!userId) {
+    throw new BadRequestError('Please provide an id');
+  }
+
+  const user = await UsersSchema.findOneAndUpdate(
+    { _id: userId },
+    { email, password },
+    { returnDocument: "after" }
+  );
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: {userId: user._id}, token },);
 }
 
 module.exports = {
